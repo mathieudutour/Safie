6fef9e8e62e59b2cedc619ddaedcbabeb9f52d9c
Mathieu Dutour <mathieu@dutour.me>
2015-05-30 12:49:03 +0100

    create account

--- client/controllers/login.js
+++ client/controllers/login.js
@@ -1,8 +1,50 @@
-Template.landing.events ({
-  'click .login': function(){
-    Router.goToPage(Router.Page.LOGIN);
+Template.login.events({
+  'submit': function (event, template) {
+    event.preventDefault();
+    var email = template.find('#email').value;
+    var password = template.find('#password').value;
+
+    Meteor.loginWithPassword(email, password, function(error){
+      if(error) {
+       console.log(error);
+      } else {
+        Router.goToPage(Router.Page.LANDING);
+      }
+    });
   },
-  'click .signup': function(){
-    Router.goToPage(Router.Page.SIGNUP);
+
+  'click .back': function (e) {
+    e.preventDefault();
+    Router.goToPage(Router.Page.LANDING);
+  }
+});
+
+Template.signup.events({
+  'submit': function (event, template) {
+    event.preventDefault();
+    var email = template.find('#email').value;
+    var password = template.find('#password').value;
+
+    Accounts.createUser({
+      email: email,
+      password: password,
+      profile: {
+        trusted: null,
+        name: email,
+        timeoutNotification: 3600, // 1 hour
+        timeoutDelete: 604800 // 7 days
+      }
+    }, function (error) {
+      if (error) {
+        console.log("Cannot create user");
+      } else {
+        Router.goToPage(Router.Page.LANDING);
+      }
+    });
+  },
+
+  'click .back': function (e) {
+    e.preventDefault();
+    Router.goToPage(Router.Page.LANDING);
   }
 });
