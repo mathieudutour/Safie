2f865619a1310b3e22236a057d057ad5db4e3ce8
Mathieu Dutour <mathieu@dutour.me>
2015-05-30 20:24:09 +0100

    order pictures

--- client/controllers/history.js
+++ client/controllers/history.js
@@ -11,13 +11,14 @@ Template.history.events({
 
 Template.history.helpers({
   'photos': function() {
-    return Pictures.find({userId: Meteor.userId()}, {sort: [['createdAt', -1]]}).map(function(doc, i) {
+    return Pictures.find({userId: Meteor.userId()}, {sort: [['createdAt', 'desc']]}).map(function(doc, i) {
       doc.unsafe = i === 0 && doc.triggerAt < new Date();
       return doc;
     });
   },
   'isUnsafe': function() {
-    var lastPicture = Pictures.findOne({userId: Meteor.userId()}, {sort: [['createdAt', -1]], limit: 1});
+    var lastPicture = Pictures.findOne({userId: Meteor.userId()}, {sort: [['createdAt', 'desc']], limit: 1});
+    console.log(lastPicture)
     return lastPicture && lastPicture.triggerAt < new Date();
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,6 @@
 Meteor.publish("history", function () {
   if (this.userId) {
-    return Pictures.find({userId: this.userId}, {sort: [['createdAt', -1]], fields: {data: 0}});
+    return Pictures.find({userId: this.userId}, {sort: [['createdAt', 'desc']], fields: {data: 0}});
   } else {
     this.ready();
   }
@@ -8,7 +8,7 @@ Meteor.publish("history", function () {
 
 Meteor.publish("last", function () {
   if (this.userId) {
-    return Pictures.find({userId: this.userId}, {sort: [['createdAt', -1]], limit: 1});
+    return Pictures.find({userId: this.userId}, {sort: [['createdAt', 'desc']], limit: 1});
   } else {
     this.ready();
   }
