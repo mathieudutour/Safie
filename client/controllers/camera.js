31c7796ec532a6df161ac0deb4d751e4feb4d5f2
Mathieu Dutour <mathieu@dutour.me>
2015-05-30 11:45:42 +0100

    WIP

--- client/controllers/camera.js
+++ client/controllers/camera.js
@@ -1,12 +1,33 @@
-Template.camera.onRendered(function() {
-  var canvas = this.find("#camera");
-  window.plugin.CanvasCamera.initialize(canvas);
-  var options = {
-    quality: 75,
-    destinationType: CanvasCamera.DestinationType.DATA_URL,
-    encodingType: CanvasCamera.EncodingType.JPEG,
-    width: 640,
-    height: 480
-  };
-  window.plugin.CanvasCamera.start(options);
+if (Meteor.isCordova) {
+  Meteor.startup(function() {
+    window.currentLocation = null;
+    Tracker.autorun( function() {
+      window.currentLocation = Geolocation.currentLocation();
+    });
+    var tapEnabled = false; //enable tap take picture
+    var dragEnabled = false; //enable preview box drag across the screen
+    var toBack = true; //send preview box to the back of the webview
+    var rect = {x: 0, y: 0, width: document.body.offsetWidth, height:document.body.offsetHeight};
+    cordova.plugins.camerapreview.startCamera(rect, "front", tapEnabled, dragEnabled, toBack);
+
+    // handle picture taken
+    cordova.plugins.camerapreview.setOnPictureTakenHandler(function(result){
+        document.getElementById('originalPicture').src = result[0];//originalPicturePath;
+        document.getElementById('previewPicture').src = result[1];//previewPicturePath;
+    });
+  });
+}
+
+Template.camera.events({
+  'click .reverse-camera': function() {
+    if (Meteor.isCordova) {
+      cordova.plugins.camerapreview.switchCamera();
+    }
+  },
+
+  'click .take-picture': function() {
+    if (Meteor.isCordova) {
+      cordova.plugins.camerapreview.takePicture({maxWidth:640, maxHeight:640});
+    }
+  }
 });
