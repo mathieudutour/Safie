56e365171f92e67c8b44ec9fdc7a40161a3c4bd9
Mathieu Dutour <mathieu@dutour.me>
2015-05-31 11:52:36 +0100

    fix backend

--- server/security.js
+++ server/security.js
@@ -2,27 +2,24 @@ Meteor.methods({
 	'security_code' : function(code) {
       check(code, Number);
 
-      var Future = Npm.require('fibers/future');
-      var future = new Future();
-
-      var user = Meteor.users.findOne(this.userId);
-      var lastPicture = Pictures.findOne({userId: this.userId}, {sort: [['createdAt', -1]], limit: 1});
-      if(parseInt(user.profile.code) === code) {
-        Pictures.update({_id: lastPicture._id}, {$set: {triggered: false}});
-        future.return({success: true});
-      } else {
-        Pictures.update({_id: lastPicture._id}, {$set: {triggered: true}});
-        if (user.profile.trusted) {
-          Email.send({
-            from: 'watchguard@safie.com',
-            subject: '[Safie] Could check on ' + user.profile.name +'?',
-            to: user.profile.trusted,
-            html: EmailTemplate.check(user, lastPicture)
-          });
+      return Meteor.wrapAsync(function() {
+        var user = Meteor.users.findOne(this.userId);
+        var lastPicture = Pictures.findOne({userId: this.userId}, {sort: [['createdAt', -1]], limit: 1});
+        if(parseInt(user.profile.code) === code) {
+          Pictures.update({_id: lastPicture._id}, {$set: {triggered: false}});
+          return {success: true};
+        } else {
+          Pictures.update({_id: lastPicture._id}, {$set: {triggered: true}});
+          if (user.profile.trusted) {
+            Email.send({
+              from: 'watchguard@safie.com',
+              subject: '[Safie] Could check on ' + user.profile.name +'?',
+              to: user.profile.trusted,
+              html: EmailTemplate.check(user, lastPicture)
+            });
+          }
+          return {success: true};
         }
-        future.return({success: false});
-      }
-
-      return future.wait();
+      });
 	},
 });
